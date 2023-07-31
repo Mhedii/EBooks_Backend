405af908464c9cdff8fd2ebdfddae84ce56e9499
Mhedii <mehedi1234.mh@gmail.com>
2023-07-31 21:29:47 +0600

    [Done] Connection Successful

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './app/routes';
+import httpStatus from 'http-status';
+
+const app: Application = express();
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api/v1', routes);
+// Testing
+app.get('/', (req: Request, res: Response) => {
+  res.send('Connction Successful');
+});
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  Promise.reject(new Error('Unhandled Promise Rejection'));
+});
+
+//Global Error Handling
+// app.use(globalErrorHandler);
+
+
+
+// Not Found Route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'NOT FOUND',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API NOT FOUND',
+      },
+    ],
+  });
+  next();
+});
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { Server } from 'http';
+import { errorLogger, logger } from './shared/logger';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// let server: Server;
+// async function bootstrap() {
+//   try {
+//     await mongoose.connect(config.database_url as string);
+//     server = app.listen(config.port, () => {
+//       console.log(`Application listening on port ${config.port}`);
+//     });
+//   } catch (err) {
+//     console.log('Connection Failed');
+//   }
+// }
+
+let server: Server;
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log('Database Connection Successfully');
+    server = app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log('failed to connect to the database', err);
+  }
+  process.on('unhandledRejection', error => {
+    console.log(
+      'Unhandled Rejection from server,We are closing the server.......',
+    );
+    if (server) {
+      server.close(() => {
+        console.log(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+process.on('SIGTERM', () => {
+  console.log('SIGTER is received');
+  if (server) {
+    server.close();
+  }
+});
